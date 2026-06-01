52565c2db14ab43d235905320c63651eca15dacb
vitneshimmanuvel <vitneshimmanuvel@gmail.com>
2026-06-01 13:07:31 +0530

    comited

--- src/pages/AdminBoards.jsx
+++ src/pages/AdminBoards.jsx
@@ -3,7 +3,7 @@ import { Plus, Edit2, Users, Check, X, Shield, Settings } from 'lucide-react';
 import api from '../utils/api';
 import { useAuth } from '../context/AuthContext';
 import { format } from 'date-fns';
-//csfsfd egfdaffqefqf svdsddsfaafaadvdC AVCCsvsvbsvsvvadcadczvdfadcZVZVXZCdva
+//csfsfd egfdaffqefqf svdsddsfaafaadvdC AVCCsvsvbsvsvvadcadczvdfadcZVZVXZCdvacx xc
 export default function AdminBoards() {
   const { user } = useAuth();
   const [boards, setBoards] = useState([]);
